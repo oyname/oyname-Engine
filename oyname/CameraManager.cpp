6480347ab3f4a9bd7d65934d52cab91b27ea1284
oyname <zamansiz74@gmail.com>
2024-05-01 11:53:52 +0200

    CameraManager

---
+++ oyname/CameraManager.cpp
@@ -0,0 +1,80 @@
+#include "CameraManager.h"
+
+namespace gdx
+{
+    CameraManager::CameraManager() : m_currentCam(nullptr), m_engine(nullptr)
+    {
+        m_projMode = PERSPECTIVE_LH;
+        m_ortho = { 0 };
+        m_viewport = { 0 };
+        m_perspective = { 0 };
+    }
+
+    CameraManager::~CameraManager()
+    { }
+
+    void CameraManager::Init(gdx::CGIDX* engine)
+    {
+        m_projMode = PERSPECTIVE_LH;
+        SetViewport(0, 0, 0, 0, 0, 0);
+        SetPerspective(0.0f, 0.0f, 0.0f, 0.0f);
+        SetOrthographic(0.0f, 0.0f, 0.0f, 0.0f);
+        m_engine = engine;
+    }
+
+    void CameraManager::SetViewport(float TopLeftX, float TopLeftY, float Width, float Height, float MinDepth, float MaxDepth)
+    {
+        m_viewport.TopLeftX = TopLeftX;
+        m_viewport.TopLeftY = TopLeftY;
+        m_viewport.Width = Width;
+        m_viewport.Height = Height;
+        m_viewport.MinDepth = MinDepth;
+        m_viewport.MaxDepth = MaxDepth;
+    }
+
+    void CameraManager::SetPerspective(float fieldOfView, float aspectRatio, float nearPlane, float farPlane)
+    {
+        m_perspective.fieldOfView = fieldOfView;
+        m_perspective.aspectRatio = aspectRatio;
+        m_perspective.nearPlane = nearPlane;
+        m_perspective.farPlane = farPlane;
+    }
+
+    void CameraManager::SetOrthographic(float width, float height, float nearPlane, float farPlane)
+    {
+        m_ortho.width = width;
+        m_ortho.height = height;
+        m_ortho.nearPlane = nearPlane;
+        m_ortho.farPlane = farPlane;
+    }
+
+    void CameraManager::CreateCamera(LPMESH* camera)
+    {
+        if (m_projMode == PERSPECTIVE_LH)
+        {
+            (*camera)->cb.projectionMatrix = XMMatrixPerspectiveFovLH(m_perspective.fieldOfView, m_perspective.aspectRatio, m_perspective.nearPlane, m_perspective.farPlane);
+        }
+        else if (m_projMode == ORTHO_LH)
+        {
+            (*camera)->cb.projectionMatrix = XMMatrixOrthographicLH(m_ortho.width, m_ortho.height, m_ortho.nearPlane, m_ortho.farPlane);
+        }
+
+        (*camera)->cb.viewMatrix = XMMatrixLookAtLH((*camera)->transform.getPosition(), (*camera)->transform.getLookAt(), (*camera)->transform.getUp());
+        (*camera)->cb.worldMatrix = XMMatrixIdentity();
+    }
+
+    D3D11_VIEWPORT CameraManager::GetViewPort()
+    {
+        return m_viewport;
+    }
+
+    LPMESH CameraManager::GetCurrentCam()
+    {
+        return m_currentCam;
+    }
+
+    void CameraManager::SetCamera(LPMESH camera)
+    {
+        m_currentCam = camera;
+    }
+}
---
+++ oyname/CameraManager.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "gdxutil.h"
+#include "ObjectManager.h"
+
+namespace gdx
+{
+	class CameraManager
+	{
+		enum PROJ_MODE
+		{
+			ORTHO_LH,
+			PERSPECTIVE_LH,
+		};
+
+		struct Perspective
+		{
+			float fieldOfView;
+			float aspectRatio;
+			float nearPlane;
+			float farPlane;
+		};
+
+		struct Ortho
+		{
+			float width;
+			float height;
+			float nearPlane;
+			float farPlane;
+		};
+
+	public:
+		CameraManager();
+		~CameraManager();
+
+		void Init(gdx::CGIDX* engine);
+
+		void SetViewport(float TopLeftX, float TopLeftY, float Width, float Height, float MinDepth, float MaxDepth);
+		void SetPerspective(float fieldOfView, float aspectRatio, float nearPlane, float farPlane);
+		void SetOrthographic(float width, float height, float nearPlane, float farPlane);
+		void SetCamera(LPMESH mesh);
+
+		LPMESH GetCurrentCam();
+
+		void CreateCamera(LPMESH* camera);
+
+		D3D11_VIEWPORT GetViewPort();
+
+	private:
+		gdx::CGIDX* m_engine;
+		PROJ_MODE m_projMode;
+		D3D11_VIEWPORT m_viewport;
+		Perspective m_perspective;
+		Ortho m_ortho;
+		LPMESH m_currentCam;
+	};
+}
